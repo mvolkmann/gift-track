c66a9a0935895b6c01bbeb82e1b04f02dc02780d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-08-09 17:44:43 -0500

    progress on adding people

--- src/routes/people.svelte
+++ src/routes/people.svelte
@@ -1,18 +1,43 @@
 <script>
-  const people = ['Tami', 'Amanda', 'RC', 'Jeremy', 'Meghan'];
+  import Dialog from '$lib/Dialog.svelte';
+
+  const people = [
+    'Tami',
+    'Amanda',
+    'RC',
+    'Jeremy',
+    'Meghan',
+    'Super Long Name'
+  ];
   people.sort();
+
+  let dialog;
+
+  function addPerson() {
+    dialog.showModal();
+  }
 </script>
 
 <section class="people">
-  <h2>People</h2>
+  <h2>
+    People
+    <button class="bare" on:click={addPerson}>➕</button>
+  </h2>
   {#each people as person}
-    <button class="person">{person}</button>
+    <div class="person-row">
+      <button class="person truncate">{person}</button>
+      <button class="bare">🗑</button>
+    </div>
   {/each}
 </section>
 
+<Dialog bind:dialog title="Add Person">
+  Add form for adding a person here.
+</Dialog>
+
 <style>
   h2 {
-    margin-top: 0;
+    margin: 0 0 0.5rem 0;
   }
 
   .people {
@@ -22,5 +47,20 @@
     gap: 0.5rem;
 
     outline: 1px dashed red;
+  }
+
+  .person-row {
+    display: flex;
+    align-items: center;
+  }
+
+  .person-row > button {
+    width: 5rem;
+  }
+
+  .truncate {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
 </style>
